dbf1549d8c1e8931074c575fa0d640d1fc724a2c
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-08-22 12:13:52 +0900

    Edit chat_channle.rb

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,12 +1,13 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "chat_channel"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 
-  def speak
+  def speak(data)
+    ActionCable.server.broadcast 'chat_channel', message: data['message']
   end
 end
